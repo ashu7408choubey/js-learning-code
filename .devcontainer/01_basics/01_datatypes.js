c52c2ef9bb907862e51d06870bf379d1b67ed3a6
ashu7408choubey <147411639+ashu7408choubey@users.noreply.github.com>
2025-08-07 17:38:09 +0000

    datatype

---
+++ .devcontainer/01_basics/01_datatypes.js
@@ -0,0 +1,31 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) // we are using nodejs, not browser it only works on browser
+
+console.log(3 
+    + 3) // code readability should be high
+
+console.log("Ayush")
+
+let name  = "Ayush"
+let age = 21
+let isLoggedIn = false
+let state ;
+
+// number => 2 to the power of 53
+// bigint => for large integer
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value not defined
+// symbol => for uniqueness of components 
+
+
+// object
+
+
+console.log(typeof age); // number it is type 
+
+console.log(typeof null); // object it is type for null may language mistake 
+
+console.log(typeof undefined) // undefined type 
