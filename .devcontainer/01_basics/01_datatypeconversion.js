ce0f4a07265bc32ed6dbc97e6aad5a65e9f0b201
ashu7408choubey <147411639+ashu7408choubey@users.noreply.github.com>
2025-08-08 06:24:00 +0000

    conversion of datatypes

---
+++ .devcontainer/01_basics/01_datatypeconversion.js
@@ -0,0 +1,36 @@
+let score = "Ayush";
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score);
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0;
+
+//let isLoggedIn = 1
+
+//let isLoggedIn = "";
+
+//let isLoggedIn = ""
+
+let isLoggedIn = "Ayush";
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+//console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false;
+// "" => false;
+//"Ayush" => true;
+
+let num = 45;
+
+let stringNum = String(num);
+
+console.log(stringNum);
+
+console.log(typeof stringNum);
