c3cd462262d90b3e320d7d74df21438775d78148
ashu7408choubey <147411639+ashu7408choubey@users.noreply.github.com>
2025-08-09 08:00:39 +0000

    heap and stack

--- .devcontainer/01_basics/datatypes-summary.js
+++ .devcontainer/01_basics/datatypes-summary.js
@@ -28,6 +28,42 @@ const myFunction = function(){
     console.log("Hello World");
 }
 
-console.log(typeof myFunction);
-console.log(myFunction);
+// console.log(typeof myFunction);
+// console.log(myFunction);
+
+
+// *****************************************************************************
+
+// stack (Primitive), Heap (Non-Primitive)
+
+let name = "Aman";
+let anotherName = name;
+
+console.log(name);
+//console.log(anotherName);
+
+anotherName = "Ayush";
+console.log(anotherName);
+
+let userOne = {
+    email: "Aman@gmail.com",
+    upi: "aman@ybl"
+}
+
+let userTwo = userOne;
+
+console.log(userOne);
+
+console.log(userTwo);
+
+userTwo.email = "Ayush@gmail.com";
+userTwo.upi = "Ayush@ybl";
+
+console.log(userOne.email);
+
+console.log(userTwo.upi);
+
+
+// for primitive type we get the copy of the previous object so , there will be no change in the second object
+//for Non-primitve type we use heap memory means we take the reference of other object and it change while changing other.
 
